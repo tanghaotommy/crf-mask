50745b5f480fad9ed7eab83d5bf102d164bb3e12
Hao Tang <297942868@qq.com>
2018-08-21 17:13:51 -0700

    Added watershed to evaluate

--- evaluate.py
+++ evaluate.py
@@ -1,10 +1,52 @@
 import os, sys
+import cv2
+import numpy as np
 sys.path.append(os.path.dirname(__file__))
 
 from train_0 import *
 
 ##--------------------------------------------------------------
 
+def watershed(im):
+    """
+    Watershed algorithm for seperating each nuclei
+    im: probability of the pixel being a nuclei
+    return: each pixel labeled with integer indicating individual nucleis
+    """
+    # Watershed
+    im = (im * 255).astype(np.uint8)
+
+    ret, thresh = cv2.threshold(im,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+
+    kernel = np.ones((3,3),np.uint8)
+    opening = cv2.morphologyEx(thresh,cv2.MORPH_OPEN,kernel, iterations = 2)
+
+
+    sure_bg = cv2.dilate(opening,kernel,iterations=3)
+    sure_bg = sure_bg
+
+    # Finding sure foreground area
+    # dist_transform = cv2.distanceTransform(opening,cv2.DIST_L2,5)
+    # ret, sure_fg = cv2.threshold(dist_transform,0.7*dist_transform.max(),255,0)
+    eroded_img = cv2.erode(im, kernel, iterations=3)
+    sure_fg = ((eroded_img > 200) * 255).astype(np.uint8)
+
+    # Finding unknown region
+    sure_fg = np.uint8(sure_fg)
+    unknown = cv2.subtract(sure_bg, sure_fg)
+
+    ret, markers = cv2.connectedComponents(sure_fg)
+    markers = markers+1
+    markers[unknown==255] = 0
+
+    im = im[:,:,np.newaxis]
+    im = np.repeat(im, 3, axis=-1)
+    im = im.astype(np.uint8)
+
+    markers = cv2.watershed(im,markers)
+    im[markers == -1] = [255,0,0]
+    
+    return markers
 
 ## overwrite functions ###
 def revert(net, images):
